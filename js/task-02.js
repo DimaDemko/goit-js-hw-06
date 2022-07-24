1b02802dff21403f0bf1d24f01c3e71a71fca7d7
DimaDemko <idmitriy4g@gmail.com>
2022-07-24 18:49:23 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsElem = document.querySelector("#ingredients");
+const elements = [];
+
+for (let item of ingredients) {
+  const elem = document.createElement("li");
+  elem.textContent = item;
+  elem.className = "item";
+  elements.push(elem);
+}
+
+ingredientsElem.append(...elements);
